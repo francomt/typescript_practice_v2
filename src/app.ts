67452774ac887b30110ebe6a755918900ba8efb5
Franco Trelles <franco.trellesm@gmail.com>
2021-01-22 16:08:37 -0500

    Renders map with input from user using axios & Maps API

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import axios from "axios";
 
+// declare var google: any;
+
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
 
 const GOOGLE_API_KEY = "";
+
+type GoogleGeocodingResponse = {
+  data: {
+    results: { geometry: { location: { lat: number; lng: number } } }[];
+    status: "OK";
+  };
+};
 
 async function searchAddressHandler(event: Event) {
   event.preventDefault();
@@ -11,14 +20,29 @@ async function searchAddressHandler(event: Event) {
   const enteredAddress = addressInput.value;
 
   try {
-    const response = await axios.get(
+    const response: GoogleGeocodingResponse = await axios.get(
       `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
         enteredAddress
       )}&key=${GOOGLE_API_KEY}`
     );
 
-    console.log(response);
+    if (response.data.status !== "OK") {
+      throw new Error("Could not fetch location");
+    }
+
+    const coordinates = response.data.results[0].geometry.location;
+
+    const map = new google.maps.Map(
+      document.getElementById("map") as HTMLDivElement,
+      {
+        center: coordinates,
+        zoom: 8,
+      }
+    );
+
+    const marker = new google.maps.Marker({ position: coordinates, map: map });
   } catch (error) {
+    alert(error.message);
     console.error(error);
   }
 }
